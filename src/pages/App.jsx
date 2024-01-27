12613750b410dfc707c98e95376ef7759678579f
Chris Lorenzo <christopher_lorenzo@comcast.com>
2024-01-27 16:58:38 -0500

    add announcer

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,23 @@
 import { useNavigate } from "@solidjs/router";
 import { View } from "@lightningjs/solid";
-import { useFocusManager } from "@lightningjs/solid-primitives";
+import { useFocusManager, useAnnouncer } from "@lightningjs/solid-primitives";
 
 const App = (props) => {
   useFocusManager({
+    Announcer: 'a',
     Menu: 'm',
     Text: 't',
     Buttons: 'b',
   });
+  
   const navigate = useNavigate();
+  const announcer = useAnnouncer();
+  announcer.debug = false;
+  announcer.enabled = false;
 
   return (
     <View ref={window.APP}
+      onAnnouncer={() => announcer.enabled = !announcer.enabled}
       onLast={() => history.back()}
       onText={() => navigate('/text')}
       onButtons={() => navigate('/buttons')}

f98149d2c3db4889cc640a86697bd51b4e72bab8
Chris Lorenzo <christopher_lorenzo@comcast.com>
2024-05-15 20:11:38 -0400

    add legacy build plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url';
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [importChunkUrl(), solidPlugin({
@@ -8,7 +9,12 @@ export default defineConfig({
       moduleName: "@lightningjs/solid",
       generate: 'universal',
     },
-  })],
+  }),
+  legacy({
+    targets: ["defaults", "Chrome >= 49"],
+    // additionalLegacyPolyfills: ["whatwg-fetch", "es6-proxy-polyfill"],
+  }),
+  ],
   resolve: {
     alias: {
       theme: '@lightningjs/l3-ui-theme-base',
